047075c91db463737fa42ea8a97d64240b9d9300
Rudolph Dees <rudydees96@gmail.com>
2023-05-20 20:56:56 -0400

    More Python Script work
    The script now outputs to a csv with all game data from the previous season. This can only be modified by changing the hard coded URL. I will fix that in the future. Now it is time to move onto the R side of the project and see what we can figure out about this data.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# R_Project
+Seeing if there is a correlation between points scored and time of year along with other statistics
+    What I will use
+        NHL API Data
+        Graphs
+        Potentially SQL and Python for data cleaning
+        URL for NHL API "https://github.com/dword4/nhlapi#game-ids"
--- data_gathering_nhl.py
+++ data_gathering_nhl.py
@@ -6,21 +6,71 @@
 
 import requests
 import json
-import pandas
+import pandas as pd
 import csv
 
 
-URL_FOR_GAME_DATA_GATHERING = "https://statsapi.web.nhl.com/api/v1/schedule?startDate=2022-06-01&endDate=2023-06-01&?expand=schedule.linescore"
-VALID = "<Response [200]>"
+URL_FOR_GAME_DATA_GATHERING = "https://statsapi.web.nhl.com/api/v1/schedule?startDate=2021-08-01&endDate=2022-08-01&?expand=schedule.linescore"
+
+# DICT ATTRIBUTES
+DATES = "dates"
+GAMES = "games"
+GAME_ID = "gamePk"
+GAME_DATE = "date"
+GAME_TYPE = "gameType"
+
+
+
+# FILE LOCATIONS
+GAME_DATA_CSV = "outputs/game_data.csv"
+
+class HockeyGame:
+    def __init__(self, gameID, gameDate, homeTeam, awayTeam, homeScore, awayScore, totalScore, winningTeam, losingTeam, gameType):
+        self.gameID = gameID
+        self.gameDate = gameDate 
+        self.homeTeam =homeTeam
+        self.awayTeam =  awayTeam
+        self.homeScore =  homeScore
+        self.awayScore =  awayScore
+        self.totalScore =  totalScore
+        self.winningTeam =  winningTeam
+        self.losingTeam =  losingTeam
+        self.gameType = gameType
 
 def get_game_score_data():
+    gameList = []
     #This will be where we gather all the game score data from the NHL API
-    result = requests.get(URL_FOR_GAME_DATA_GATHERING)
-    print(result)
-    if result is VALID:
-        request_json = json.loads(result.content)
-        print("We did it")
+    try:
+        result = requests.get(URL_FOR_GAME_DATA_GATHERING)
+    except:
+        # Request failed
+        print(f"Get response failed!")
+        exit()
+    # Request was successful
+    print("Get request was successful. Loading data into json object")
+    request_json = json.loads(result.content)
+    for date in request_json[DATES]:
+        gameDate = date[GAME_DATE]
+        for game in date[GAMES]:
+            gameID = game[GAME_ID]
+            homeTeam = game["teams"]["home"]["team"]["name"]
+            awayTeam = game["teams"]["away"]["team"]["name"]
+            homeScore = game["teams"]["home"]["score"]
+            awayScore = game["teams"]["away"]["score"]
+            totalScore = homeScore + awayScore
+            if homeScore > awayScore:
+                winningTeam = homeTeam
+                losingTeam = awayTeam
+            else:
+                winningTeam = awayTeam
+                losingTeam = homeTeam
+            gameType = game[GAME_TYPE]
+            gameList.append(HockeyGame(gameID, gameDate, homeTeam, awayTeam, homeScore, awayScore, totalScore, winningTeam, losingTeam, gameType))
+    gameDataFrame = pd.DataFrame([game.__dict__ for game in gameList])
+    gameDataFrame.to_csv(GAME_DATA_CSV, index=False)
+
+        
+
 
 
 get_game_score_data()
-print("hello")
